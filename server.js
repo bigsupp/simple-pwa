6a5d7e09404f19ae3a8d051d36ffcfe5ec6ac6ec
Supapong Putthayangkura <bigsupp@gmail.com>
2019-05-10 15:41:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.delete('/del_user', function (req, res) {
 
 
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT, function () {
 
   var host = server.address().address
   var port = server.address().port
